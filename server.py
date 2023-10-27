d6db939964e91ccf4b4e9e7b3f0e97b6bcb24c57
iyabc <juliennepanes@gmail.com>
2023-10-27 20:30:17 +0800

    added database, to add kappa

--- server.py
+++ server.py
@@ -1,23 +1,23 @@
 from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS
-from keras import layers, models, optimizers, callbacks
+from keras import models
+import sqlite3
 
 from model_files import tokenizer_vars as tv
 from model_files import LSTM_class
 from model_files import constants as c
 
+import database
+
 app = Flask(__name__)
-CORS(app, resources={
-    r"/api/prediction": 
-    {
-        "origins": ["http://localhost:3000", "https://chef-blstm.onrender.com"],
-        "allow_headers": ["Content-Type"]
-    }
-})
+CORS(app)
 
 model_emb = models.load_model('model/')
 tokenizer = tv.getTokenizer()
 max_len = 230
+
+database.delete_table('evaluations_table')
+database.create_table()
 
 @app.route('/')
 def show_docs():
@@ -42,6 +42,29 @@ def predict(ingredients):
     pred_emb = LSTM_class.ModelPredict(model_emb, tokens_ind_prefix, input_prefix, max_len, embedding=True)
 
     return pred_emb.generate_sequence(temperature=1)
+
+def get_database_connection():
+    return sqlite3.connect('evaluations.db')
+
+
+@app.route('/api/eval', methods=['POST'])  
+def get_evaluation():
+    data = request.json.get('evaluator')
+    name = data.get('name')
+    email = data.get('email')
+    contact_number = data.get('contact_number')
+    profession = data.get('profession')
+    scores = data.get('scores')
+
+    if not name or not email or not contact_number or not profession or not scores:
+        return jsonify({"error": "Invalid data provided!"}), 400
+    
+    print(data)
+
+    database.insert_row(name, email, contact_number, profession, scores)
+    database.view_table()
+
+    return jsonify({"prediction": data})
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
